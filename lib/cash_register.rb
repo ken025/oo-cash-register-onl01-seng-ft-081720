1ade7d72f243695da6577ba3e06c91d63126b51f
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-28 05:04:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+attr_accessor :total, :discount, :quantity, :price
 
+ def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+    @price = price
+    @quantity = quantity
+    @transactions = []
+  end
+  
+  def add_item(title, price, quantity = 1)
+    @total += price*quantity
+    @transactions << price
+     i = quantity
+    until i == 0 do
+      @items << title
+    i -= 1
+  end
+end 
+  def apply_discount
+  
+end 
