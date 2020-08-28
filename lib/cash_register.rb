6b32bc8f37085366717fa3865f4abe14f2cf1692
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-28 05:14:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,5 +20,19 @@ attr_accessor :total, :discount, :quantity, :price
   end
 end 
   def apply_discount
-  
+   if @discount == 0
+      "There is no discount to apply."
+    else
+      self.total -= (0.01 * @discount * @total).to_i
+      "After the discount, the total comes to $#{self.total}."
+    end
+  end
+
+  def items
+    @items
+  end
+
+  def void_last_transaction
+    self.total = @total - @transactions.pop
+  end
 end 
